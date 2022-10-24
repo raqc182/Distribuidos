436d1d7bee51d6ae43240452ef9250167e06f60a
OscarEB <oscarestrada404@gmail.com>
2022-10-24 14:52:56 -0500

    Actualizado corriendo

--- clase11/Ejercicio16.java
+++ clase11/Ejercicio16.java
@@ -1,17 +1,18 @@
-public class Main implements Runnable {​
+public class Ejercicio16 implements Runnable{
     public static int compartida, n;
-    public void run() {​
+    public void run() {
         for(int i = 0; i < n; ++i) modifica();
-    }​
-    public synchronized void modifica() {​
+    }
+    public synchronized void modifica() {
         if(Thread.currentThread().getId() == 15) compartida--;
         else compartida++;
-    }​
-    public static void main(String[] args) {​
+    }
+    
+    public static void main(String[] args){
         n = Integer.parseInt(args[0]);
         // Runnable target
-        try {​
-            Main t = new Main();
+        try{
+            Ejercicio16 t = new Ejercicio16();
         // create threads
         Thread t1 = new Thread(t, "First Thread");
         Thread t2 = new Thread(t, "Second Thread");
@@ -21,8 +22,9 @@ public class Main implements Runnable {​
         t1.join();
         t2.join();
         System.out.println(compartida);
-        }​ catch (Exception e) {​
-            // TODO: handle exception
-        }​
-    }​
-}​
+        } 
+        catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
